605fb046de15b64b5e20c1745b4aca5ad72ba6c4
tynchtykkg <tynchtyk.estebesuulu@gmail.com>
2023-11-21 09:12:51 +0600

    first commit

---
+++ bin/functions_lesson.dart
@@ -0,0 +1,5 @@
+import 'package:functions_lesson/functions_lesson.dart' as functions_lesson;
+
+void main(List<String> arguments) {
+  print('Hello world: ${functions_lesson.calculate()}!');
+}
---
+++ test/functions_lesson_test.dart
@@ -0,0 +1,8 @@
+import 'package:functions_lesson/functions_lesson.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
